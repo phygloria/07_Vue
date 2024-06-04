dc97c0aae82f6c74b0c7226104cc7ecb36cb3f38
phygloria <phygloria@gmail.com>
2024-06-04 18:15:14 +0900

    now

---
+++ 08_Router/08_02_Router-basic/src/views/01_Router/PathVariable.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {onMounted, ref} from 'vue';
+import {useRoute} from 'vue-router';
+//url 리소스를 가져올 수 있게 해주는 객체
+
+const route = useRoute();
+
+onMounted(()=>{
+    console.log(route.params);
+})
+</script>
+
+<template>
+    <h1>PathVariable : {{ $route.params.id }}</h1>
+</template>
